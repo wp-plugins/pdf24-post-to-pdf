f7d5e71fb95d05bfe1e1556eca4ad67ad0b12b71
StefanZiegler <StefanZiegler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-20 12:46:44 +0000

    tagging version 2.3.5
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-post-to-pdf@243417 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.3.5/lang/el.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['pdf24PluginLang'] = array(
+	'enterEmail'	=> 'Εισάγετε τη διεύθυνση ηλεκτρονικού ταχυδρομείου',
+	'send'			=> 'Αποστέλω',
+	'postAsPdf'		=> 'Αποστολή άρθρου ως PDF για να',
+	'postsAsPdf'	=> 'Αποστολή αντικείμενα σε PDF',
+	'blTexts'		=> 'PDF | PDF Creator | Word PDF | PDF Converter | PDF Online'
+);
+	
+?>

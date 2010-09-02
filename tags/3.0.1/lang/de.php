de6f75da30815dcf8b1ed6e496f6643093afdb14
StefanZiegler <StefanZiegler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-02 12:39:07 +0000

    Tagging version 3.0.1
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-post-to-pdf@284124 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.1/lang/de.php
@@ -0,0 +1,16 @@
+<?php
+
+global $pdf24Plugin;
+
+$pdf24Plugin['lang'] = array(
+	'enterEmail'			=> 'Emailadresse',
+	'send'					=> 'Senden',
+	'sendArticleAsPDF'		=> 'Sende Artikel als PDF an',
+	'sendArticlesAsPDF'		=> 'Sende Artikel als PDF an',
+	'blTexts'				=> 'PDF | PDF erstellen | PDF Drucker | PDF Creator | PDF Download',
+	'downloadArticleAsPDF'	=> 'Download Artikel als PDF',
+	'downloadArticlesAsPDF'	=> 'Download Artikel als PDF',
+	'createPDF'				=> 'Erstelle PDF'
+);
+	
+?>

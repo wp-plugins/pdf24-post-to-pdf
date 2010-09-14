51a43f0c3ffe6c843c6a9426a61335d866518ef0
StefanZiegler <StefanZiegler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-14 10:17:29 +0000

    tagging version 3.0.2
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-post-to-pdf/tags/3.0.2@289193 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lang/it.php
+++ lang/it.php
@@ -1,11 +1,16 @@
 <?php
 
-$GLOBALS['pdf24PluginLang'] = array(
-	'enterEmail'	=> "Inserisci l'indirizzo email",
-	'send'			=> "Inviare",
-	'postAsPdf'		=> "Invia l'articolo in formato PDF a",
-	'postsAsPdf'	=> "Invia articoli in formato PDF a",
-	'blTexts'		=> "PDF | PDF Creator | PDF Free | Word PDF | PDF Download"
+global $pdf24Plugin;
+
+$pdf24Plugin['lang'] = array(
+	'enterEmail'			=> "Inserisci l'indirizzo email",
+	'send'					=> "Inviare",
+	'sendArticleAsPDF'		=> "Invia l'articolo in formato PDF a",
+	'sendArticlesAsPDF'		=> "Invia articoli in formato PDF a",
+	'blTexts'				=> "PDF | PDF Creator | PDF Free | Word PDF | PDF Download",
+	'downloadArticleAsPDF'	=> "Scarica l'articolo in formato PDF",
+	'downloadArticlesAsPDF'	=> "Scarica articoli in formato PDF",
+	'createPDF'				=> "Creare PDF"
 );
 	
 ?>

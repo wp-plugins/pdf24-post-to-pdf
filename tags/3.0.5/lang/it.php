68972f6aaa88af7c454d74a79ae5d6c43c40ee2d
pdf24 <pdf24@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-15 08:27:45 +0000

    Tagging version 3.0.5
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-post-to-pdf@300297 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.5/lang/it.php
@@ -0,0 +1,16 @@
+<?php
+
+global $pdf24Plugin;
+
+$pdf24Plugin['lang'] = array(
+	'enterEmail'			=> "Inserisci l'indirizzo email",
+	'send'					=> "Inviare",
+	'sendArticleAsPDF'		=> "Invia l'articolo in formato PDF a",
+	'sendArticlesAsPDF'		=> "Invia articoli in formato PDF a",
+	'blTexts'				=> "PDF | PDF Creator | PDF Free | Word PDF | PDF Download",
+	'downloadArticleAsPDF'	=> "Scarica l'articolo in formato PDF",
+	'downloadArticlesAsPDF'	=> "Scarica articoli in formato PDF",
+	'createPDF'				=> "Creare PDF"
+);
+	
+?>

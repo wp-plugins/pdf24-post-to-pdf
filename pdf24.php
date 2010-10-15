9d31a70c6b668a155cbfea533d0203a30e49916f
pdf24 <pdf24@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-15 08:28:54 +0000

    Final version 3.0.5
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-posts-to-pdf/trunk@300299 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pdf24.php
+++ pdf24.php
@@ -4,7 +4,7 @@ Plugin Name: PDF24 Articles To PDF
 Plugin URI: http://www.pdf24.org
 Description: A plugin that converts articles to PDF. Visitors of your blog can make a copy of articles in form of a PDF. Contents in the PDF are linked with your blog.
 Author: Stefan Ziegler
-Version: 3.0.4
+Version: 3.0.5
 Author URI: http://www.pdf24.org
 */
 

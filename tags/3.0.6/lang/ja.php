c5da03837473f6f29e3797d0f5e8e45e88801645
StefanZiegler <StefanZiegler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-25 08:13:14 +0000

    Tagging version 3.0.6
    git-svn-id: https://plugins.svn.wordpress.org/pdf24-post-to-pdf@303891 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.6/lang/ja.php
@@ -0,0 +1,16 @@
+<?php
+
+global $pdf24Plugin;
+
+$pdf24Plugin['lang'] = array(
+	'enterEmail'			=> 'メールアドレスを入力します',
+	'send'					=> '送る',
+	'sendArticleAsPDF'		=> 'PDFファイルとして送信記事',
+	'sendArticlesAsPDF'		=> 'PDF形式で記事を送信する',
+	'blTexts'				=> 'PDF | PDFファイル作成 | PDFファイルを作成する | PDFプリンタ',
+	'downloadArticleAsPDF'	=> 'PDFファイルとしてダウンロードの記事',
+	'downloadArticlesAsPDF'	=> 'PDFファイルとしてダウンロードの記事',
+	'createPDF'				=> 'PDFを作成'
+);
+	
+?>
---
+++ tags/3.0.6/lang/ru.php
@@ -0,0 +1,16 @@
+<?php
+
+global $pdf24Plugin;
+
+$pdf24Plugin['lang'] = array(
+	'enterEmail'			=> 'Введите адрес электронной почты',
+	'send'					=> 'Отправлять',
+	'sendArticleAsPDF'		=> 'Отправить статью как PDF',
+	'sendArticlesAsPDF'		=> 'Отправить статей в PDF',
+	'blTexts'				=> 'PDF | PDF Printer | PDF Editor | PDF Word | PDF Download',
+	'downloadArticleAsPDF'	=> 'Скачать статью в PDF',
+	'downloadArticlesAsPDF'	=> 'Скачать статьи в PDF',
+	'createPDF'				=> 'Создать PDF'
+);
+	
+?>
